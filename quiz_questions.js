2b05148eefadda1753cb8954fa1d9669fc8f7aa1
OUALIID <walidelhadim8@gmail.com>
2023-07-04 20:30:28 +0100

    Quiz Site

---
+++ quiz_questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "When was the first day we got to know each other?",
+        answer: "2. February 20 2023",
+        options: [
+            "1. February 15 2023",
+            "2. February 20 2023",
+            "3. February 16 2023",
+            "4. February 2 2023"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The first day we met each other?",
+        answer: "4. April 9 2023",
+        options: [
+            "1. April 21 2023",
+            "2. April 14 2023",
+            "3. April 30 2023",
+            "4. April 9 2023"
+        ]
+    },
+    {
+        numb: 3,
+        question: "In the future, can I hold your hand?",
+        answer: "1. You can",
+        options: [
+            "1. You can",
+            "2. You can not",
+            "3. I need to think",
+            "4. I will kill you"
+        ]
+    },
+    {
+        numb: 4,
+        question: "How do you consider me?",
+        answer: "3. Life partner",
+        options: [
+            "1. Your lover",
+            "2. Your friend",
+            "3. Life partner",
+            "4. Nothing"
+        ]
+    },
+    {
+        numb: 5,
+        question: "To whom you can confess our relationship",
+        answer: "2. Person from the group",
+        options: [
+            "1. One of your family",
+            "2. Person from the group",
+            "3. Your friend Khawla or Douha",
+            "4. Another person"
+        ]
+    }
+];
+
